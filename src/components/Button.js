4c9ccebe395f6d610295ea06ea76808f11cb1ad6
Sonia Singh <sonia.singh0409@gmail.com>
2022-01-18 16:09:59 -0500

    refractoring Buttons

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,18 +1,21 @@
 import React from "react";
+import classNames from "classnames";
 
 import "components/Button.scss";
 
 export default function Button(props) {
-  let buttonClass = "button";
-  if (props.confirm) {
-    buttonClass += " button--confirm";
-  }
-  if (props.danger) {
-    buttonClass += " button--danger";
-  }
-  if (props.onClick) {
-    buttonClass += "-clicked";
-  }
+  let buttonClass = classNames("button", {
+    "button--confirm": props.confirm,
+    "button--danger": props.danger,
+  });
 
-  return <button className={buttonClass} onClick={props.onClick} disabled={props.disabled}>{props.children}</button>;
+  return (
+    <button
+      className={buttonClass}
+      onClick={props.onClick}
+      disabled={props.disabled}
+    >
+      {props.children}
+    </button>
+  );
 }
