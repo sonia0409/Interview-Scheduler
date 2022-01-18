00e7e9316665d471d17f6411c63862567d65ecd1
Sonia Singh <sonia.singh0409@gmail.com>
2022-01-18 15:12:50 -0500

    conditional css to each button

--- src/components/Button.js
+++ src/components/Button.js
@@ -3,5 +3,16 @@ import React from "react";
 import "components/Button.scss";
 
 export default function Button(props) {
-   return <></>;
+  let buttonClass = "button";
+  if (props.confirm) {
+    buttonClass += " button--confirm";
+  }
+  if (props.danger) {
+    buttonClass += " button--danger";
+  }
+  if (props.onClick) {
+    buttonClass += "-clicked";
+  }
+
+  return <button className={buttonClass} onClick={props.onClick} disabled={props.disabled}>{props.children}</button>;
 }
