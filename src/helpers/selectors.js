f05ad53c611f5ffc5e96124c724533ae72ce6328
Sonia Singh <sonia.singh0409@gmail.com>
2022-01-21 17:38:10 -0500

    new function to getInterview

--- src/helpers/selectors.js
+++ src/helpers/selectors.js
@@ -1,9 +1,31 @@
 export function getAppointmentsForDay(state, day) {
-  const appointmentIds = state.days.filter(
-    (schedule) => schedule.name === day
-  ).map(day => day.appointments).flat();
-  
-  const appointments = appointmentIds.map(appointmentId => state.appointments[appointmentId])
-   
+  const appointmentIds = state.days
+    .filter((schedule) => schedule.name === day)
+    .map((day) => day.appointments)
+    .flat();
+
+  const appointments = appointmentIds.map(
+    (appointmentId) => state.appointments[appointmentId]
+  );
+
   return appointments;
+}
+
+export function getInterview(state, interview) {
+  //input state - an arrya of objects; interview- an object with student and interviewer object
+  /* const interviewObj = interview
+    ? { ...interview, interviewer: state.interviewers[interview.interviewer] }
+    : null; */
+
+  if (!interview) {
+    return null;
+  }
+  const interviewId = interview.interviewer
+  const interviewObj = {
+    student: interview.student,
+    interviewer: state.interviewers[interviewId]
+  };
+
+  //output object of interview with student and interviewer = state.interviewer
+  return interviewObj;
 }
